b738db6a96547436a7212a77d1d608474daceb60
Franck W <franck.rj.w@gmail.com>
2020-04-13 16:14:20 +0200

    Màj du main pour utiliser la nouvelle classe.

--- hvlov-server/src/main.cpp
+++ hvlov-server/src/main.cpp
@@ -4,25 +4,38 @@
 #include <spdlog/spdlog.h>
 
 #include "FileSystemLister.hpp"
+#include "HvlovEntryBuilder.hpp"
 
-int main()
+int main(int argc, char** argv)
 {
+    spdlog::set_level(spdlog::level::level_enum::debug);
+
+    if (argc < 2 || argv[1][0] == '\0')
+    {
+        spdlog::error("Server root not passed as first param of command.");
+        return 1;
+    }
+
+    const std::string serverRoot = argv[1];
     const std::string serverAddress = "localhost";
     constexpr int serverPort = 1234;
 
-    spdlog::set_level(spdlog::level::level_enum::debug);
-
     std::unique_ptr<hvlov::IFileSystemLister> fileSystemLister = std::make_unique<hvlov::FileSystemLister>();
+    hvlov::HvlovEntryBuilder::Config hvlovEntryBuilderConfig{serverRoot};
+    std::unique_ptr<hvlov::IHvlovEntryBuilder> hvlovEntryBuilder =
+        std::make_unique<hvlov::HvlovEntryBuilder>(hvlovEntryBuilderConfig);
 
     httplib::Server server;
 
     server.Post("/", [&](const httplib::Request& req, httplib::Response& res) {
         std::string response;
-        std::vector<hvlov::FileInfo> entries;
+        std::vector<hvlov::HvlovEntry> entries;
+
+        spdlog::info("Request '{}' received.", req.body);
 
         try
         {
-            entries = fileSystemLister->getEntriesFromDirectory(req.body);
+            entries = hvlovEntryBuilder->buildEntriesFromFileInfos(fileSystemLister->getEntriesFromDirectory(req.body));
         }
         catch (const std::exception& e)
         {
@@ -33,13 +46,17 @@ int main()
 
         for (auto&& entry : entries)
         {
-            spdlog::debug(entry.path.string());
-            response += entry.path.string() + "\n";
+            std::string result = fmt::format("<{0} url=\"{1}\">{2}</{0}>",
+                                             (entry.type == hvlov::HvlovEntry::Type::Folder ? "folder" : "video"),
+                                             entry.url.toString(), entry.title);
+
+            spdlog::debug(result);
+            response += result + "\n";
         }
 
         res.set_content(response, "text/plain");
     });
 
-    spdlog::info("HVlov server start listening on {}:{}", serverAddress, serverPort);
+    spdlog::info("HVlov server start listening on {}:{} with '{}' as root", serverAddress, serverPort, serverRoot);
     server.listen(serverAddress.c_str(), serverPort);
 }
