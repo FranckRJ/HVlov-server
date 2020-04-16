be73259810fe65c86e9ebf7e029c1557cbc68aed
Franck W <franck.rj.w@gmail.com>
2020-04-16 16:18:25 +0200

    Ajout de prérequis globaux au projet en plus de celui de target (c++17).

--- cmake/SetupGlobalCMakeFlags.cmake
+++ cmake/SetupGlobalCMakeFlags.cmake
@@ -1,3 +1,8 @@
+# Set C++ standard requirements
+set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(CMAKE_CXX_EXTENSIONS OFF)
+
 # Remove default MSVC warning flags because it will be set later (if asked)
 if (MSVC)
     string(REPLACE "/W3" "" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
