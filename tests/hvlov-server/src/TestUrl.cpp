c2284543733769e5a385ccee9597505704af121a
Franck W <franck.rj.w@gmail.com>
2020-04-17 14:08:00 +0200

    Ajout de tests pour la classe Url.

---
+++ tests/hvlov-server/src/TestUrl.cpp
@@ -0,0 +1,73 @@
+#include <catch2/catch.hpp>
+
+#include "Url.hpp"
+
+using namespace hvlov;
+
+SCENARIO("Url::toString()", "[unit]")
+{
+    GIVEN("A constructed URL from a string")
+    {
+        std::string urlAsString = "something";
+        Url url{urlAsString};
+
+        WHEN("The toString() method is called")
+        {
+            const std::string& returnedString = url.toString();
+
+            THEN("A copy of the URL passed as parameter of the constructor is returned")
+            {
+                REQUIRE(returnedString == urlAsString);
+                REQUIRE(&returnedString != &urlAsString);
+            }
+        }
+
+        WHEN("The toString() method is called multiple times")
+        {
+            const std::string& firstReturnedString = url.toString();
+            const std::string& secondReturnedString = url.toString();
+
+            THEN("Each call return a different string (but with the same value)")
+            {
+                REQUIRE(firstReturnedString == secondReturnedString);
+                REQUIRE(&firstReturnedString != &secondReturnedString);
+            }
+        }
+    }
+}
+
+SCENARIO("Url::operator==() / Url::operator!=()", "[unit]")
+{
+    GIVEN("Two URL constructed from the same string")
+    {
+        std::string urlAsString = "something";
+        Url firstUrl{urlAsString};
+        Url secondUrl{urlAsString};
+
+        WHEN("They are compared")
+        {
+            THEN("They are equals")
+            {
+                REQUIRE(firstUrl == secondUrl);
+                REQUIRE_FALSE(firstUrl != secondUrl);
+            }
+        }
+    }
+
+    GIVEN("Two URL constructed from two non-equals strings")
+    {
+        std::string firstUrlAsString = "something";
+        std::string secondUrlAsString = "other_thing";
+        Url firstUrl{firstUrlAsString};
+        Url secondUrl{secondUrlAsString};
+
+        WHEN("They are compared")
+        {
+            THEN("They are not equals")
+            {
+                REQUIRE(firstUrl != secondUrl);
+                REQUIRE_FALSE(firstUrl == secondUrl);
+            }
+        }
+    }
+}
