e4a8a7c8c9cb34ee1035448101522e80430d619a
Franck W <franck.rj.w@gmail.com>
2020-04-17 12:56:34 +0200

    Affichage constant du buildtype.

--- cmake/StandardProjectSettings.cmake
+++ cmake/StandardProjectSettings.cmake
@@ -4,6 +4,8 @@ if (NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
     set(CMAKE_BUILD_TYPE RelWithDebInfo CACHE STRING "Choose the type of build." FORCE)
     # Set the possible values of build type for cmake-gui, ccmake
     set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release" "MinSizeRel" "RelWithDebInfo")
+else ()
+    message(STATUS "Build type was set to '${CMAKE_BUILD_TYPE}'.")
 endif ()
 
 find_program(CCACHE ccache)
