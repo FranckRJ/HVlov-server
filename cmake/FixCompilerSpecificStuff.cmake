cb506365df3a21b09981499108a4f9c8bf8aa959
Franck W <franck.rj.w@gmail.com>
2020-04-18 18:37:41 +0200

    Fix d'un bug appliquant le fix de GCC8 pour la lib fs à tout compilateur

--- cmake/FixCompilerSpecificStuff.cmake
+++ cmake/FixCompilerSpecificStuff.cmake
@@ -4,7 +4,9 @@ if (MSVC)
 endif ()
 
 # Link stdc++fs for GCC 8 (required)
-if (CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 8 AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS 9)
+if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND
+        CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 8 AND
+        CMAKE_CXX_COMPILER_VERSION VERSION_LESS 9)
     target_link_libraries(project_global_options INTERFACE stdc++fs)
     message(STATUS "Added stdc++fs library because GCC 8 was detected.")
 endif ()
