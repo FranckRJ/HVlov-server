23eb840d1c2be663c781a6507fa938cf076d0eed
Franck W <franck.rj.w@gmail.com>
2020-06-06 17:45:37 +0200

    Correction d'une lib manquante sous Windows.

--- cmake/FixCompilerSpecificStuff.cmake
+++ cmake/FixCompilerSpecificStuff.cmake
@@ -3,10 +3,8 @@ if (MSVC)
     string(REPLACE "/W3" "" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
 endif ()
 
-# Link stdc++fs for GCC 8 (required)
-if (CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND
-        CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 8 AND
-        CMAKE_CXX_COMPILER_VERSION VERSION_LESS 9)
-    target_link_libraries(project_global_options INTERFACE stdc++fs)
-    message(STATUS "Added stdc++fs library because GCC 8 was detected.")
+# Link ws2_32 for Windows with GCC (required for cpp-httplib)
+if (WIN32 AND CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    target_link_libraries(project_global_options INTERFACE ws2_32)
+    message(STATUS "Added ws2_32 library because Windows with GCC was detected.")
 endif ()
